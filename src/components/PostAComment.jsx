b25f2f9f9bfdb305549e2458a3b0b15cdc709464
Anna Chernova <annanvks@gmail.com>
2023-03-16 17:02:36 +0000

    add some css

--- src/components/PostAComment.jsx
+++ src/components/PostAComment.jsx
@@ -14,21 +14,32 @@ const PostAComment = (props) => {
       body: event.target[0].value,
     };
 
-    postComment(article_id, newComment).then((newCommentPosted) => {
-      props.setComments([newCommentPosted, ...props.comments]);
-    });
+    // postComment(article_id, newComment).then((newCommentPosted) => {
+    //   props.setComments([newCommentPosted, ...props.comments]);
+    // });
   };
 
   return (
-    <form className="comment_adder" onSubmit={handleSubmit}>
+    <form className="form_container" onSubmit={handleSubmit}>
       <h2>What do you think?</h2>
-      <label htmlFor="newComment">Add a comment</label>
-      <textarea
-        id="newComment"
-        // value={newComment}
-        // onChange={(event) => setNewComment(event.targer.value)}
-      />
-      <button type="submit">Add a comment</button>
+      <div className="comment_adder">
+        <label id="label_username" htmlFor="input_username">
+          Username
+        </label>
+        <input type="text" placeholder="Enter Username" id="input_username" />
+        <label id="label_textarea" htmlFor="newComment">
+          Add a comment
+        </label>
+        <textarea
+          id="newComment"
+          placeholder="Add your comment here..."
+          // value={newComment}
+          // onChange={(event) => setNewComment(event.targer.value)}
+        />
+        <button id="button_new_comment" type="submit">
+          Add a comment
+        </button>
+      </div>
     </form>
   );
 };
